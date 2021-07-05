5ff617242077f433ab27ec2c960855b69635b625
Dean Alvarez <dean.alvarez.24@gmail.com>
2021-07-04 23:57:53 -0400

    add full file output

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import random
 import rsa
 import base64
+from io import StringIO  
 
 import discord
 from dotenv import load_dotenv
@@ -15,6 +16,33 @@ client.voting = False
 client.votes = []
 client.key = 1
 
+# Given votes and the decrypting key, this function generates two strings.
+# summary will contain how many votes each option got.
+# full_output will contain votes and identifiers but not the random noise.
+def count_votes_and_generate_output(vote_list,priv_key):
+    file_output = ""
+    summary_output = ""
+    votes = {}
+    for vote in vote_list:
+        snack = base64.b64decode(vote.encode("ascii"))
+        dec_vote = rsa.decrypt(snack,priv_key)
+        decoded = dec_vote.decode("ascii").split('-')
+        file_output += decoded[0] + "-" + decoded[1]
+        file_output += "\n"
+        vote_key = decoded[0].strip().lower()
+
+        if vote_key in votes:
+            votes[vote_key] += 1
+        else:
+            votes[vote_key] = 1
+
+    for key in votes:
+        summary_output += "{} got {} votes".format(key,votes[key])
+        summary_output += "\n"
+
+    return (summary_output,file_output)
+
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
@@ -26,16 +54,20 @@ async def on_message(message):
 
     if '!!endvote' in message.content:
         if client.voting:
-            client.voting = False
+            
+            #shuffle to remove original ordering
             random.shuffle(client.votes)
-            output = ""
-            for vote in client.votes:
-                snack = base64.b64decode(vote.encode("ascii"))
-                dec_vote = rsa.decrypt(snack,client.key)
-                output += dec_vote.decode("ascii")
-                output += "\n"
+
+            #count votes and prepare outputs
+            summary_out,file_out = count_votes_and_generate_output(client.votes,client.key)
+            out_file = discord.File(StringIO(file_out),filename="results.txt")
+
+            #flush data from voting session
             client.key = 1
-            await message.channel.send(output)
+            client.votes = []
+            client.voting = False
+
+            await message.channel.send(content=summary_out,file=out_file)
         else:
             await message.channel.send("A vote has not been started.")
 
@@ -46,7 +78,8 @@ async def on_message(message):
             client.voting = True
             pub_key, client.key = rsa.newkeys(1024)
             output = """
-            A vote has been started! Use the key below and https://8gwifi.org/rsafunctions.jsp to send in your vote. \n\n
+            @here A vote has been started! Use the key below and https://8gwifi.org/rsafunctions.jsp to send in your vote.\n
+            Format your vote like this vote-identifier-random_padding\n\n
             """ + "Here is the key:\n"+pub_key.save_pkcs1().decode("utf-8")
             
             await message.channel.send(output)
